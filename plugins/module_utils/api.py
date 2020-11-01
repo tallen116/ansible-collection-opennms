840f0bafb722dfde1e3b2cf2b1e4e6c5b067398b
tallen116 <5997740+tallen116@users.noreply.github.com>
2020-11-01 17:25:11 -0500

    Add API call request

--- plugins/module_utils/api.py
+++ plugins/module_utils/api.py
@@ -5,10 +5,80 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 from .module import ONMSModule
-# from ansible.module_utils.urls import Request
+from ansible.module_utils.urls import Request, SSLValidationError, ConnectionError
+from ansible.module_utils.six.moves.urllib.error import HTTPError
+from ansible.module_utils.six import PY2
+import json
 
 
 class ONMSAPIModule(ONMSModule):
 
+    request = None
+
     def __init__(self, argument_spec, **kwargs):
         super(ONMSAPIModule, self).__init__(argument_spec=argument_spec, **kwargs)
+        self.request = Request()
+
+    def make_request(self, method, endpoint, *args, **kwargs):
+
+        if not method:
+            raise Exception("The HTTP method must be defined")
+
+        headers = {}
+        headers['Accept'] = "application/json"
+        validate_certs = self.params.get('validate_certs')
+        username = self.params.get('username')
+        password = self.params.get('password')
+        force_basic_auth = True
+        data = kwargs.get('data', None)
+        version = kwargs.get('version', 1)
+        url = self.build_url(endpoint, version=version)
+        status_code = None
+
+        try:
+            response = self.request.open(
+                method,
+                url.geturl(),
+                headers=headers,
+                url_username=username,
+                url_password=password,
+                force_basic_auth=force_basic_auth,
+                validate_certs=validate_certs,
+                data=data,
+                follow_redirects=True
+            )
+        except(SSLValidationError) as ssl_error:
+            self.fail_json(msg="Could not establish a secure connection to {0}: {1}".format(self.url.geturl(), ssl_error))
+        except(HTTPError) as he:
+            if he.code >= 500:
+                self.fail_json(msg="The host responded with a server error ({1}): {0}".format(url.netloc, he.code))
+            if he.code == 401:
+                self.fail_json(msg="An authentication error has occured with user: {0}".format(username))
+            if he.code == 403:
+                self.fail_json(msg="A forbidden request was sent to the host ({1}): {0}".format(url.netloc, he.msg))
+            if he.code == 404:
+                self.fail_json(msg="The requested resource does not exist at {0}".format(url.path))
+            if he.code == 405:
+                self.fail_json(msg="The host responded that method {0} is not allowed to this endpoint {1}".format(method, url.path))
+        except(Exception) as e:
+            self.fail_json(msg="There was an unknown error when calling {0}: {1}.".format(self.url.geturl(), e))
+
+        response_body = ''
+
+        try:
+            response_body = response.read()
+        except(Exception) as e:
+            self.fail_json(msg="Failed to read response body: {0}".format(e))
+
+        response_json = {}
+        if response_body and response_body != '':
+            try:
+                response_json = json.loads(response_body)
+            except(Exception) as e:
+                self.fail_json(msg="Failed to parse the response json: {0}".format(e))
+
+        if PY2:
+            status_code = response.getcode()
+        else:
+            status_code = response.status
+        return {'status_code': status_code, 'json': response_json}
--- plugins/module_utils/module.py
+++ plugins/module_utils/module.py
@@ -19,12 +19,14 @@ class ONMSModule(AnsibleModule):
         validate_certs=dict(type='bool')
     )
 
+    # Define defaults
     host = '127.0.0.1'
     username = None
     password = None
     validate_certs = True
 
     def __init__(self, argument_spec=None, **kwargs):
+        self._debug = True
         full_argspec = {}
         full_argspec.update(ONMSModule.AUTH_ARGSPEC)
         full_argspec.update(argument_spec)
@@ -34,12 +36,12 @@ class ONMSModule(AnsibleModule):
         super(ONMSModule, self).__init__(full_argspec, **kwargs)
 
         self.host = self.params['host']
+        self.debug(msg="The host provided is {}".format(self.host))
 
+        # Validate
         if not re.match('^https?://', self.host):
             self.fail_json(msg="URL Scheme is expecting http or https.")
 
-        # Basic validation
-        # Validate url structure
         try:
             self.url = urlparse(self.host)
         except Exception as e:
@@ -47,13 +49,29 @@ class ONMSModule(AnsibleModule):
 
         hostname = self.url.netloc.split(':')[0]
         try:
-            gethostbyname(hostname)
+            self._resolvehost = gethostbyname(hostname)
+            self.debug(msg="Host resolved to {}".format(self._resolvehost))
         except Exception as e:
             self.fail_json(msg="Unable to resolve host ({1}): {0}".format(hostname, e))
 
-    def build_url(self, interface, query_params=None):
+    def build_url(self, endpoint, query_params=None, version=1):
         """
         Build the URL for the REST calls.
         Base URL is http(s)://opennms:8980/opennms/rest/
+        Base V2 URL is http(s)://opennms:8980/opennms/rest/v2/
         """
-        pass
+        if not endpoint.startswith("/"):
+            endpoint = "/{0}".format(endpoint)
+        if version < 1:
+            self.fail_json(msg="Invalid API version provided")
+        if version > 1:
+            endpoint = "/v{1}{0}".format(endpoint, version)
+        if not endpoint.startswith("/rest/") and version == 1:
+            endpoint = "/rest{0}".format(endpoint)
+        if not endpoint.startswith("/api/") and version > 1:
+            endpoint = "/api{0}".format(endpoint)
+        if not endpoint.startswith("/opennms/"):
+            endpoint = "/opennms{0}".format(endpoint)
+
+        url = self.url._replace(path=endpoint)
+        return url
